61a21779f89598cc6ad46b897314a66006b7267e
Travis <burnett.travis@gmail.com>
2019-12-20 21:55:30 -0600

    challenge 1

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 /**********************************************
 * 3. FCC Mongo & Mongoose Challenges
 * ==================================
@@ -13,7 +14,7 @@
 // as MONGO_URI. Connect to the database using the following syntax:
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 /** # SCHEMAS and MODELS #
